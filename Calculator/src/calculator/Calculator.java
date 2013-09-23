f920d935eb5ad781afd7ece16e0b6ec1091d21c0
Ivan Ivashkovskiy <ivan290895@yandex.ru>
2013-09-23 21:59:28 +0400

    ver 1.1  normal_code

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,89 +1,145 @@
 package calculator;
-import java.util.Scanner;
 
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Calculator {
-    public static int calculate(String convert_string) {
+
+	public static String convert17(int number10) {
+		String result = new String();
+		int tempNum = number10;
+		while(tempNum > 0) {
+			int mod = tempNum % 17;
+			if(mod < 10)
+				result = (char)('0' + mod) + result;
+			else
+				result = (char)('A' + mod - 10) + result;
+			tempNum /= 17;
+		}
+		return result;
+	}
+
+	public static int priority(char operator) {
+        switch(operator) {
+            case '(': return 0;
+            case ')': return 1;
+            case '+': return 2;
+            case '-': return 2;
+            case '*': return 3;
+            case '/': return 3;
+            default: return 0;
+        }
+    }
+
+    public static String toPostfix(String inputString) {
+        StringBuilder sb = new StringBuilder();
+    	String outputString;
+        LinkedList<Character> operators = new LinkedList<>();
+        int i = 0;
+        while(i < inputString.length()) {
+            char token = inputString.charAt(i);
+            if((token >= '0' && token <='9') || (token >= 'A' && token <= 'G')) {
+                sb.append(token);
+                ++i;
+                continue;
+            }
+            if(operators.isEmpty()) {
+            sb.append(' ');
+               operators.push(token);
+               ++i;
+               continue;
+            }
+            if(token == '(') {
+                operators.push(token);
+                ++i;
+                continue;
+            }
+            if(token == ')') {
+            	sb.append(' ');
+                while(operators.getFirst() != '(' ) {
+                    sb.append(operators.getFirst());
+                    operators.pop();
+                }
+                operators.pop();
+                ++i;
+                continue;
+            }
+            sb.append(' ');
+            while(!operators.isEmpty() && (priority(operators.getFirst()) >= priority(token))) {
+            	sb.append(operators.getFirst());
+                operators.pop();
+            }
+            operators.push(token);
+            ++i;
+        }
+        sb.append(' ');
+        while(!operators.isEmpty()) {
+            sb.append(operators.pop());
+        }
+        outputString = sb.toString();
+        return outputString;
+    }
+
+	public static int getNumber(String convertString, int i) {
+	        int num = 0;
+	        int length = convertString.length()-1;
+	        while(i < length && convertString.charAt(i) != ' ') {
+	            if(convertString.charAt(i) >= '0' && convertString.charAt(i) <= '9')
+	                num = num * 17 + convertString.charAt(i) - '0';
+	            else
+	                num = num * 17 + convertString.charAt(i) - 'A' + 10;
+	            ++i;
+	        }
+	        return num;
+	    }
+
+    public static int calculate(String convertString) {
         int result = 0;
         LinkedList<Integer> numbers = new LinkedList<>();
-        int i = 0;
-        while(i < convert_string.length()) {
-            char token = convert_string.charAt(i);
-            //System.out.println(token);
+        for (int i = 0; i < convertString.length(); ++i) {
+            char token = convertString.charAt(i);
             if((token >= '0' && token <='9') || (token >= 'A' && token <= 'G')) {
-                int temp = getNumber(convert_string,i);
-                //System.out.println(numbers.getFirst());
+                int temp = getNumber(convertString, i);
                 numbers.push(temp);
-                //System.out.println(numbers.getFirst());
-                //System.out.println(numbers.getLast());
-                ++i;
                 continue;
             }
             if(token == '+') {
-                //System.out.println(result);
-                //System.out.println(numbers.getLast());
-                int temp = numbers.pop()+ numbers.pop();
-                //System.out.println(temp);
-                //System.out.println(temp);
-                //numbers.pop();
+                int temp = numbers.pop() + numbers.pop();
                 numbers.push(temp);
-                //System.out.println(numbers.getLast());
-                ++i;
                 continue;
             }
             if(token == '/') {
-                int temp = numbers.pop() / numbers.pop();
+                int temp = numbers.pop();
+                temp =  numbers.pop() / temp;
                 numbers.push(temp);
-                ++i;
                 continue;
             }
             if(token == '-') {
                 int temp = numbers.pop() - numbers.pop();
-                numbers.push(temp);
-                ++i;
+                numbers.push(-temp);
                 continue;
             }
             if(token == '*') {
                 int temp = numbers.pop() * numbers.pop();
-                //System.out.println(temp);
                 numbers.push(temp);
-                ++i;
                 continue;
-            }
-            if(token == ' ') {
-                //System.out.println("-");
-                ++i;
             }
         }
         return numbers.getLast();
     }
-    public static void main(String[] args) {
-        String input_string;
-        Scanner in;
-        in = new Scanner(System.in);
-        input_string = in.nextLine();
-        while(!(input_string.equals("end"))) {
-            //System.out.println(input_string);
-            String convert_string;
-            int result;
-            convert_string = Convert.toPostfix(input_string);
-            //System.out.println(convert_string);
-            result = calculate(convert_string);
-            System.out.println(result);
-            input_string = in.nextLine();
-        }
-    }
-    public static int getNumber(String convert_string, int i) {
-        int num = 0;
-        while(i < convert_string.length()-1 && convert_string.charAt(i) != ' ') {
-            if(convert_string.charAt(i) >= '0' && convert_string.charAt(i) <= '9')
-                num = num * 17 + convert_string.charAt(i) - '0';
-            else
-                num = num * 17 + convert_string.charAt(i) - 'A' + 10;
-            ++i;
-            //System.out.println(num);
-        }
-        return num;
-    }
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		for (String s : args) {
+			sb.append(s);
+		}
+		String expression = sb.toString();
+
+		String convertString = toPostfix(expression);
+		int result = calculate(convertString);
+		String outStr = convert17(result);
+		System.out.println(outStr);
+
+		System.exit(0);
+	}
 }
